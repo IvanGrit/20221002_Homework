be8868e551d29f2706d107c852aa02b62597a445
Ivan Gr <of_2000@mail.ru>
2022-10-03 02:00:35 +0400

    Task25

---
+++ 01_Task25/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 25: Напишите метод, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B с использованием цикла.
+
+using static System.Console;
+Clear();
+WriteLine("Введите число A: ");
+int a = int.Parse(ReadLine()!);
+WriteLine("Введите число B: ");
+int b = int.Parse(ReadLine()!);
+WriteLine(RaseToDegree(a,b));
+
+
+
+
+
+int RaseToDegree(int i, int j)
+{
+    int count = 1;
+    int result = i;
+    while (count < j)
+    {
+        result *= i;
+        count++;
+    }
+    return result;
+}
