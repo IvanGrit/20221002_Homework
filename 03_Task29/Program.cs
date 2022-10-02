86a069dcbec65bc5e2683e0321d1d8608ad75361
Ivan Gr <of_2000@mail.ru>
2022-10-03 02:01:05 +0400

    Task29

---
+++ 03_Task29/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 29: Напишите метод, который формирует массив из 8 случайных элементов и выводит их на экран.
+
+using static System.Console;
+Clear();
+int[] array = getArray(15);
+printArray(array);
+
+
+
+
+
+
+int[] getArray(int length)
+{
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+     result[i] = new Random().Next(-100,101);
+    
+    }
+    return result;
+}
+
+void printArray(int[] array3)
+{
+    string FinalString = "[ ";
+    foreach (int item in array3)
+    {
+        FinalString+=$"{item} ";
+    }
+    FinalString += "]";
+    WriteLine(FinalString);
+}
